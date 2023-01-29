a8b2ab94fc6fe7ace59a0c54b516671bfe51f860
Gucchiy <gucchiy@gmail.com>
2023-01-30 01:04:31 +0900

    added hello_world2 and db exported file

---
+++ html/ew.2.3.hello_world2.php
@@ -0,0 +1,14 @@
+<?php
+define('WP_USE_THEMES', true);
+
+$_GET['p'] = 1;
+$_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
+$_SERVER['REQUEST_METHOD'] = 'GET';
+$_SERVER['REQUEST_URI'] = '/?p=1';
+$_SERVER['HTTP_HOST'] = 'localhost:8000';
+
+require_once( 'wp-load.php' );
+wp();
+require_once( ABSPATH . WPINC . '/template-loader.php' );
+
+var_dump( $wp_filter['wp'], $wp_filter['the_title'] );
